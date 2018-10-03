eb25ca8ab21f4e900ebb1f147a6031e4d871ad14
TheHipsterCriollo <jose-dgr-13@hotmail.com>
2018-10-03 15:23:38 -0500

    resize

--- js/view.js
+++ js/view.js
@@ -47,9 +47,9 @@ const view = {
         const infoModal = document.createElement('div');
         infoModal.id = 'infoModal';
         infoModal.innerHTML = `
-        <div class="bg" style="position: fixed; z-index: 5; top: 0; left: 0; background: black; opacity: 0.6; width: 100vw; height: 100vh"></div>
+        <div class="bg" style="position: fixed;top: 0;left: 0;background: black;opacity: 0.6;width: 100vw;height: 100vh;z-index: 5;"></div>
         <div class='content'>
-        <span id="close" style="font-family: Gotham; font-size: 2.5em">X</span>
+        <span id="close">X</span>
         <h1>${info.title}</h1>
         ${info.info}
         </div>
@@ -91,7 +91,7 @@ const view = {
         project.innerHTML = `
         <div class="bg" style="position: fixed; top: 0; left: 0; background: black; opacity: 0.6; width: 100vw; height: 100vh"></div>
         <div class="content">
-            <span id="close" style="font-family: Gotham; font-size: 2.5em">X</span>
+            <span id="close">X</span>
             <h1 style="font-size: 3em">${projectReceive.title}</h1>
             <div class="table">
                 <div class="col">
